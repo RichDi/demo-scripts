c6731f25f0831767047cbf28c9af9b055e5d2db0
RichDi <drdr_2@hotmail.com>
2024-11-25 18:08:55 -0600

    Initial commit with tests

---
+++ test/index.test.js
@@ -0,0 +1,14 @@
+import sum from '../index.js';
+import assert from 'assert';
+
+describe('sum', () => {
+  it('should handle positive numbers', () => {
+    assert.equal(sum(1, 2), 3);
+  });
+  it('should handle negative numbers', () => {
+    assert.equal(sum(-3, -5), -8);
+  });
+  it('should handle decimals', () => {
+    assert.equal(sum(0.3, 0.7), 1);
+  });
+});
